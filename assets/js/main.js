df5db59f9ad1932ad1621b6fe316730995d7ae7e
achim <astrohm@gmx.net>
2016-03-19 18:26:08 +0100

    modified weather

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,27 @@
 
 document.addEventListener("DOMContentLoaded", init, false); //Event wird geworfen, wenn DOM-Baum geladen wurde. es wird dann die init-Methode aufgerufen
 
-
+var weatherRequest;
 
 function init(){
 	//alert("init aufgerufen");
+
 	showTime();
 	showDate();
 	showWeather();
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,9 +1,84 @@
 
 function showWeather(){
+    //alert("showWeather aufgerufen");
 	gettingWeatherJSON();
 }
 
+
+
+
+
+
+
+
+
+
 function gettingWeatherJSON(){
+        
+
+        var url = "api.openweathermap.org/data/2.5/forecast?q=weingarten,de&APPID=c6831ebb9b3db6cde29c3e08edb49fa8"; //koordinaten weingarten
+
+        if (window.XMLHttpRequest){ // code for IE7+, Firefox, Chrome, Opera, Safari
+            // create AJAX-Request-Object
+            request = new XMLHttpRequest();
+            
+            // Kommunikation mit Server initialisieren
+            request.open("GET", url, true);
+            
+            // Eventhandler registrieren, um auf asynchrone Antwort vom Server reagieren zu können
+            request.onreadystatechange = weatherResponseFromOpenweather;
+            
+            // Anfrage senden
+            request.send();     
+    } else { // code for IE6, IE5, non AJAX compatible browsers
+        alert("Browser unterstützt kein AJAX. IE7+, Firefox, Chrome, Opera, Safari oder ein anderer AJAX-fähriger Browser notwendig!");
+    }   
+
+}
+
+
+
+
+function weatherResponseFromOpenweather(){
+    //anfrage nur alle 10 min, sonst keine antwort
+
+    if(request.readyState == 4){
+        
+        //var weatherData = request.responseText;
+        //alert(request.responseText.city.name);
+        //var cityName = JSON.parse(weatherData);
+        alert(JSON.stringify(request.responseText));
+        alert(JSON.parse(request.responseText));
+        //alert(cityName);
+
+       
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+        /*
 
 		var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22sacramento%2C%20ca%22)&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
         
@@ -37,10 +112,10 @@ function gettingWeatherJSON(){
             alert("Aktuelle Temperatur: " + currentTemperature);
             alert("Maximale Temperatur: " + maxTemperature);
             alert("Minimale Temperatur: " + minTemperature);
-            */
-        });
+            
+        
 
-}
+}*/
 
 
 
